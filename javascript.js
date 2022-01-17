9645b5870e5de18810294a6a45797387a0353754
Jainex17 <81921291+Jainex17@users.noreply.github.com>
2022-01-17 14:07:00 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let bollyactArrow = document.querySelector(".bollyact-arrow");
+bollyactArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+}
+let cbArrow = document.querySelector(".cb-arrow");
+cbArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+}
